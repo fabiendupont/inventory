32b434a3946e4484043d17fde1581ced82655025
Fabien Dupont <fdupont@evenit.info>
2020-12-23 19:49:13 +0100

    Checkpoint

--- src/main/java/io/konveyor/forklift/inventory/vmware/DistributedVirtualPortGroup.java
+++ src/main/java/io/konveyor/forklift/inventory/vmware/DistributedVirtualPortGroup.java
@@ -1,20 +1,17 @@
 package io.konveyor.forklift.inventory.vmware;
 
-import java.util.Set;
-
 import javax.persistence.Entity;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 
 @Entity
-public class Network extends PanacheEntity {
+public class DistributedVirtualPortGroup extends PanacheEntity {
     
     public String Name;
 
-    @ManyToMany(targetEntity = Host.class)
-    public Set<Host> hostSet;
+    @ManyToOne
+    public Datacenter datacenter;
 
     @ManyToOne
     public Folder folder;
---
+++ src/main/java/io/konveyor/forklift/inventory/vmware/DistributedVirtualPortGroupResource.java
@@ -0,0 +1,33 @@
+package io.konveyor.forklift.inventory.vmware;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+@Path("/distributedvirtualportgroups")
+public class DistributedVirtualPortGroupResource {
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<DistributedVirtualPortGroup> DistributedVirtualPortGroups() {
+        return DistributedVirtualPortGroup.listAll();
+    }
+
+    @Transactional
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response newDistributedVirtualPortGroup(DistributedVirtualPortGroup dvpg) {
+        dvpg.id = null;
+        dvpg.persist();
+        return Response.status(Status.CREATED).entity(dvpg).build();
+    }
+}
